61642d271ece5fdf21ded6e7b7ca67300a167db2
Robin Cade <robin.cade54@gmail.com>
2022-02-14 14:40:18 +0000

    added the GET /api/articles/article:id path

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,18 @@
 /** @format */
 
-const { selectTopics } = require('../models/model.js');
+const { selectTopics, selectArticleById } = require('../models/model.js');
 
 exports.fetchTopics = (req, res, next) => {
 	selectTopics().then((topics) => {
 		res.status(200).send({ topics });
 	});
+};
+
+exports.fetchArticle = (req, res, next) => {
+
+    const articleId = req.params.article_id
+
+    selectArticleById(articleId).then((article) => {
+			res.status(200).send({ article });
+		});
 };
--- models/model.js
+++ models/model.js
@@ -6,3 +6,10 @@ exports.selectTopics = async () => {
             `)
     return topics.rows
 }
+
+exports.selectArticleById = async (articleId) => {
+    const article = await db.query(`
+                                    SELECT * FROM articles WHERE article_id = $1
+                                    `, [articleId])
+    return article.rows[0]
+}
